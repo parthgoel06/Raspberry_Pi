f101d884182a648fe09c1afc91e3c89002ec1804
Parth Goel <parthgoel06@gmail.com>
2020-01-14 02:53:13 +0530

    face recog edit

--- pi-face-recognition/pi_face_recognition.py
+++ pi-face-recognition/pi_face_recognition.py
@@ -118,8 +118,6 @@ cv2.imshow("Frame", frame)
 key = cv2.waitKey(1) & 0xFF
 
 # if the `q` key was pressed, break from the loop
-if key == ord("q"):
-	break
 
 # update the FPS counter
 #fps.update()
