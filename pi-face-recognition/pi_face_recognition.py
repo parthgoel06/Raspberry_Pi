fc0dd72f944f7808f5d1d3b7579f4ef3092efcbb
Parth Goel <parthgoel06@gmail.com>
2020-01-14 02:48:26 +0530

    face recog edit 3

--- pi-face-recognition/pi_face_recognition.py
+++ pi-face-recognition/pi_face_recognition.py
@@ -28,93 +28,107 @@ detector = cv2.CascadeClassifier(args["cascade"])
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 #vs = VideoStream(src=0).start()
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
+#vs = VideoStream(usePiCamera=True).start()
+#time.sleep(2.0)
 
 # start the FPS counter
-fps = FPS().start()
+#fps = FPS().start()
 
 # loop over frames from the video file stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to 500px (to speedup processing)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=500)
+
+# grab the frame from the threaded video stream and resize it
+# to 500px (to speedup processing)
+camera = PiCamera()
+camera.resolution = (IM_WIDTH,IM_HEIGHT)
+camera.framerate = 10
+rawCapture = PiRGBArray(camera, size=(IM_WIDTH, IM_HEIGHT))
+rawCapture.truncate(0)
+
+time.sleep(2)
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
 	
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+# Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+# i.e. a single-column array, where each item in the column has the pixel RGB value
+frame = np.copy(image)
+frame.setflags(write=1)
+frame_expanded = np.expand_dims(frame, axis=0)
+frame = imutils.resize(frame, width=500)
 
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+# convert the input frame from (1) BGR to grayscale (for face
+# detection) and (2) from BGR to RGB (for face recognition)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# OpenCV returns bounding box coordinates in (x, y, w, h) order
-	# but we need them in (top, right, bottom, left) order, so we
-	# need to do a bit of reordering
-	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+# detect faces in the grayscale frame
+rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+	minNeighbors=5, minSize=(30, 30),
+	flags=cv2.CASCADE_SCALE_IMAGE)
 
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
+# OpenCV returns bounding box coordinates in (x, y, w, h) order
+# but we need them in (top, right, bottom, left) order, so we
+# need to do a bit of reordering
+boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown"
+# compute the facial embeddings for each face bounding box
+encodings = face_recognition.face_encodings(rgb, boxes)
+names = []
 
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+# loop over the facial embeddings
+for encoding in encodings:
+	# attempt to match each face in the input image to our known
+	# encodings
+	matches = face_recognition.compare_faces(data["encodings"],
+		encoding)
+	name = "Unknown"
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
+	# check to see if we have found a match
+	if True in matches:
+		# find the indexes of all matched faces then initialize a
+		# dictionary to count the total number of times each face
+		# was matched
+		matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+		counts = {}
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
-		
-		# update the list of names
-		names.append(name)
+		# loop over the matched indexes and maintain a count for
+		# each recognized face face
+		for i in matchedIdxs:
+			name = data["names"][i]
+			counts[name] = counts.get(name, 0) + 1
 
-	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# draw the predicted face name on the image
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 0), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			0.75, (0, 255, 0), 2)
+		# determine the recognized face with the largest number
+		# of votes (note: in the event of an unlikely tie Python
+		# will select first entry in the dictionary)
+		name = max(counts, key=counts.get)
+	
+	# update the list of names
+	names.append(name)
 
-	# display the image to our screen
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+# loop over the recognized faces
+for ((top, right, bottom, left), name) in zip(boxes, names):
+	# draw the predicted face name on the image
+	cv2.rectangle(frame, (left, top), (right, bottom),
+		(0, 255, 0), 2)
+	y = top - 15 if top - 15 > 15 else top + 15
+	cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+		0.75, (0, 255, 0), 2)
+print(name)
+# display the image to our screen
+cv2.imshow("Frame", frame)
+key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+# if the `q` key was pressed, break from the loop
+if key == ord("q"):
+	break
 
-	# update the FPS counter
-	fps.update()
+# update the FPS counter
+#fps.update()
 
 # stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+#fps.stop()
+#print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+#vs.stop()
