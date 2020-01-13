332f503dc7026a3968b099c2ca6e78d233d0b32f
Parth Goel <parthgoel06@gmail.com>
2020-01-14 00:48:52 +0530

    face recog edit 2

--- pi-face-recognition/encode_faces.py
+++ pi-face-recognition/encode_faces.py
@@ -18,7 +18,7 @@ ap.add_argument("-i", "--dataset", required=True,
 	help="path to input directory of faces + images")
 ap.add_argument("-e", "--encodings", required=True,
 	help="path to serialized db of facial encodings")
-ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+ap.add_argument("-d", "--detection-method", type=str, default="hog",
 	help="face detection model to use: either `hog` or `cnn`")
 args = vars(ap.parse_args())
 
