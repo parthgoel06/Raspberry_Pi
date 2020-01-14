a77c112cdcb6270ea8aa3582a8231b78e6839657
Parth Goel <parthgoel06@gmail.com>
2020-01-14 17:01:27 +0530

    edit

--- pi-face-recognition/pi_face_recognition.py
+++ pi-face-recognition/pi_face_recognition.py
@@ -134,16 +134,4 @@ for ((top, right, bottom, left), name) in zip(boxes, names):
 cv2.imshow("Frame", frame)
 key = cv2.waitKey(5000) & 0xFF
 
-# if the `q` key was pressed, break from the loop
-
-# update the FPS counter
-#fps.update()
-
-# stop the timer and display FPS information
-#fps.stop()
-#print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
 cv2.destroyAllWindows()
-#vs.stop()
