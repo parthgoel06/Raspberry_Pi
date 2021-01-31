94f81befd51e8b544223fb125f4b546304cceb16
Parth Goel <parthgoel06@gmail.com>
2021-02-01 04:51:07 +0530

    fixes

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -122,26 +122,41 @@ if camera_type == 'picamera':
     rawCapture.truncate(0)
     
     to_detect = 'clock'
+    try:
+        for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 
-    for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+            t1 = cv2.getTickCount()
+            
+            # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+            # i.e. a single-column array, where each item in the column has the pixel RGB value
+            frame = np.copy(frame1.array)
+            frame.setflags(write=1)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_expanded = np.expand_dims(frame_rgb, axis=0)
 
-        t1 = cv2.getTickCount()
-        
-        # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-        # i.e. a single-column array, where each item in the column has the pixel RGB value
-        frame = np.copy(frame1.array)
-        frame.setflags(write=1)
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_expanded = np.expand_dims(frame_rgb, axis=0)
+            # Perform the actual detection by running the model with the image as input
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: frame_expanded})
 
-        # Perform the actual detection by running the model with the image as input
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
-
-        # Draw the results of the detection (aka 'visulaize the results')
-        try:
-            _, detected = vis_util.visualize_boxes_and_labels_on_image_array(
+            # Draw the results of the detection (aka 'visulaize the results')
+            try:
+                _, detected = vis_util.visualize_boxes_and_labels_on_image_array(
+                    frame,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8,
+                    min_score_thresh=0.40)
+                
+                print(detected[0:-5])
+                if detected[0:-5] == to_detect:
+                    os.system("omxplayer ~/Desktop/audio/beep.mp3")
+                
+            except:
+                vis_util.visualize_boxes_and_labels_on_image_array(
                 frame,
                 np.squeeze(boxes),
                 np.squeeze(classes).astype(np.int32),
@@ -150,38 +165,26 @@ if camera_type == 'picamera':
                 use_normalized_coordinates=True,
                 line_thickness=8,
                 min_score_thresh=0.40)
-            
-            print(detected[0:-5])
-            if detected[0:-5] == to_detect:
-                os.system("omxplayer ~/Desktop/audio/beep.mp3")
-            
-        except:
-            vis_util.visualize_boxes_and_labels_on_image_array(
-            frame,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=8,
-            min_score_thresh=0.40)
-            
-        cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+                
+            # cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
 
-        # All the results have been drawn on the frame, so it's time to display it.
-        cv2.imshow('Object detector', frame)
+            # # All the results have been drawn on the frame, so it's time to display it.
+            # cv2.imshow('Object detector', frame)
 
-        t2 = cv2.getTickCount()
-        time1 = (t2-t1)/freq
-        frame_rate_calc = 1/time1
+            # t2 = cv2.getTickCount()
+            # time1 = (t2-t1)/freq
+            # frame_rate_calc = 1/time1
 
-        # Press 'q' to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
+            # # Press 'q' to quit
+            # if cv2.waitKey(1) == ord('q'):
+            #     break
 
-        rawCapture.truncate(0)
+            rawCapture.truncate(0)
+    
+    except KeyboardInterrupt:
+        break
 
     camera.close()
 
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 
